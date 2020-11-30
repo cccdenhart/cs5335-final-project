3564eddab9aa79dfed4ee1e8dcfec25bf0dd14db
psforero <ps.gomezforero@gmail.com>
2020-11-29 21:36:53 -0500

    EEPROM functions

--- ArduinoSamples/EEPROM/EEPROM.ino
+++ ArduinoSamples/EEPROM/EEPROM.ino
@@ -1,35 +1,26 @@
 # include <EEPROM.h>
+#include <math.h>
 
+const bool READ_EEPROM = true;
 const int NUM_STATES = 36;  // number of state (distance) intervals used in qtable
 const int NUM_ACTIONS = 20;  // number of action (vl - vr) intervals used in qtable
-const bool READ_EEPROM = false;
-int num = 1;
 
-// a representation of the world state
-struct WorldState {
-  float** qtable;
-};
+int TICK = 0;
 
-WorldState STATE;
+float qtable[NUM_STATES][NUM_ACTIONS] = {};
+
+
 
 // initializes the q-table
-float** init_qtable(int num_states, int num_actions, bool read_csv) {
-
-  float** qtable = new float*[num_states];
-  if (read_csv) {
-    qtable = read_EEPROM();
-    return qtable;
-  } else {
-    for (int i = 0; i < num_states; i++) {
-      qtable[i] = new float[num_actions];
-      for (int j = 0; j < num_actions; j++)
-        qtable[i][j] = (i * NUM_ACTIONS + j);
-    }
+void init_qtable() {
+  for (int i = 0; i != NUM_STATES; i++) {
+    for (int j = 0; j != NUM_ACTIONS; j++)
+      qtable[i][j] = 0.0;
   }
-  return qtable;
 }
 
-void write_EEPROM(float** qtable) {
+
+void write_EEPROM() {
   for (int i = 0; i < NUM_STATES; i++) {
     for (int j = 0; j < NUM_ACTIONS; j++) {
       EEPROM.update(i * NUM_ACTIONS + j, qtable[j][i]);
@@ -37,29 +28,9 @@ void write_EEPROM(float** qtable) {
   }
 }
 
-/*
-  void write_csv(char* filepath, float** qtable) {
-  std::ofstream csv;
-  csv.open(filepath);
+
+void read_EEPROM() {
   for (int i = 0; i < NUM_STATES; i++) {
-    float* row = qtable[i];
-    for (int j = 0; j < NUM_ACTIONS; j++) {
-      csv << row[j];
-      if (j < (NUM_ACTIONS - 1))
-        csv << ",";
-      else
-        csv << "\n";
-    }
-  }
-  }
-
-*/
-
-float** read_EEPROM() {
-  float** qtable = new float*[NUM_STATES];
-
-  for (int i = 0; i < NUM_STATES; i++) {
-    qtable[i] = new float[NUM_ACTIONS];
     for (int j = 0; j < NUM_ACTIONS; j++) {
       qtable[i][j] = EEPROM.read(i * NUM_ACTIONS + j);
     }
@@ -68,40 +39,20 @@ float** read_EEPROM() {
   return qtable;
 }
 
-/*
-  float** read_from_csv(char* filepath) {
-  float** qtable = new float*[NUM_STATES;
-  std::ifstream csv;
-  csv.open(filepath);
-  while (!csv.eof()) {
-    float* row = new float[NUM_ACTIONS];
-    string line, val;
-    getline(csv, line);
-    std::stringstream ss(line);
-    while (getline(ss, val, ',')) {
-      row.push_back(std::stof(val));
-    }
-    qtable.push_back(row);
-  }
-  return qtable;
-  }
-*/
-
 
 // update the qtable with new information
-float** update_qtable(float** qtable) {
+void update_qtable() {
 
   for (int i = 0; i < NUM_STATES; i++) {
     for (int j = 0; j < NUM_ACTIONS; j++) {
-      qtable[i][j] += 1;
+      if (random(10) > 5)
+        qtable[i][j] += random(3);
     }
   }
-
-  return qtable;
 }
 
 
-void print_qtable(float** qtable) {
+void print_qtable() {
   for (int i = 0; i < NUM_STATES; i++) {
     for (int j = 0; j < NUM_ACTIONS; j++) {
       Serial.print((int)qtable[i][j]);
@@ -113,19 +64,26 @@ void print_qtable(float** qtable) {
 }
 
 void setup() {
-  Serial.begin(9600);
+  randomSeed(analogRead(0)); // used for mock update
 
-  STATE.qtable = init_qtable(NUM_STATES, NUM_ACTIONS, READ_EEPROM);
-  
-  Serial.println("set up");
+  Serial.begin(9600);
+  if (READ_EEPROM) {
+    read_EEPROM();
+  } else {
+    init_qtable();
+  }
+
+  print_qtable();
 }
 
 void loop() {
-  STATE.qtable = update_qtable(STATE.qtable);
-  Serial.println(num);
-  if (num % 500 == 0) {
-    print_qtable(STATE.qtable);
-    write_EEPROM(STATE.qtable);
+  update_qtable();
+
+  if (TICK % 100 == 0) {
+    print_qtable();
+    Serial.println("WRITING...");
+    write_EEPROM();
   }
-  num++;
+  Serial.println(TICK);
+  TICK++;
 }
