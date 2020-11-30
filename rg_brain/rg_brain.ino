05d29a2d8c7b3eba89c4e13d3272eaf9911f90e5
cccdenhart <cccdenhart@me.com>
2020-11-29 20:29:51 -0500

    cleaned code, remove global fn params

--- rg_brain/rg_brain.ino
+++ rg_brain/rg_brain.ino
@@ -82,30 +82,7 @@ MeLightSensor light_l(PORT_12);
 MeBuzzer siren;
 #define BUZZER_PORT 45
 
-/*
-
-  // a raw representation of a robot state
-  struct QState {
-  float dist_f;
-  float dist_r;
-  };
-
-  // a raw representation of a robot action
-  struct QAction {
-  float vl;
-  float vr;
-  };
-
-  // a representation of the world state
-  struct WorldState {
-
-  };
-
-
-  WorldState STATE;
-*/
-
-void print_qtable(float** qtable) {
+void print_qtable() {
   for (int i = 0; i < NUM_STATES; i++) {
     for (int j = 0; j < NUM_ACTIONS; j++) {
       Serial.print((int)qtable[i][j]);
@@ -153,7 +130,7 @@ float clamp(float xmin, float xx, float xmax) {
 
 // converts a raw state representation to an integer representation
 // (usable by the Q-Table)
-int discretize_state(float dist_f, float dist_r) {
+int discretize_state() {
   int root_states = floor(sqrt(NUM_STATES));
 
   float dist_f_local = clamp(0.0, dist_f, RG_MAX_DIST);
@@ -167,14 +144,6 @@ int discretize_state(float dist_f, float dist_r) {
   return result;
 }
 
-// converts a raw action representation to an integer representation
-// (usable by the Q-Table)
-int discretize_action(float vl, float vr) {
-  float diff = clamp(RG_BASE_VEL + RG_MIN_VEL, vl - vr, RG_BASE_VEL + RG_MAX_VEL);
-  int norm = floor((diff - RG_MIN_VEL) / (RG_MAX_VEL - RG_MIN_VEL) * NUM_ACTIONS);
-  return norm;
-}
-
 // convert an integer action representation to a QAction
 // (usable for performing the action)
 void realize_action(int int_action) {
@@ -184,152 +153,13 @@ void realize_action(int int_action) {
   cur_vl = vl;
   cur_vr = vr;
 }
-/*void realize_action(int int_action) {
-  switch (int_action) {
-    case 0:
-      cur_vl = -170;
-      cur_vr = 250;
-      break;
-    case 1:
-      cur_vl = -120;
-      cur_vr = 230;
-      break;
-    case 2:
-      cur_vl = 100;
-      cur_vr = 210;
-      break;
-    case 3:
-      cur_vl = 120;
-      cur_vr = 190;
-      break;
-    case 4:
-      cur_vl = 100;
-      cur_vr = 170;
-      break;
-    case 5:
-      cur_vl = 230;
-      cur_vr = 230;
-      break;
-    case 6:
-      cur_vl = 170;
-      cur_vr = 100;
-      break;
-    case 7:
-      cur_vl = 190;
-      cur_vr = 120;
-    case 8:
-      cur_vl = 210;
-      cur_vr = 100;
-      break;
-    case 9:
-      cur_vl = 230;
-      cur_vr = -120;
-      break;
-    case 10:
-      cur_vl = 250;
-      cur_vr = -170;
-      break;
-    case 11:
-      cur_vl = -150;
-      cur_vr = -150;
-      break;
-  }
-  cur_vr = cur_vr * -1.0;
-  }*/
-/*void realize_action(int int_action) {
-  switch (int_action) {
-      int pwm = 220;
-    case 0:    // move forward
-      cur_vl = pwm;
-      cur_vr = -pwm;
-      break;
-    case 1:     // Hard left
-      cur_vl = pwm - pwm / 2;
-      cur_vr = -pwm;
-      break;
-    case 2:     // Hard right
-      cur_vl = pwm;
-      cur_vr = -pwm + pwm / 2;
-      break;
-    case 3:     // Veer left
-      cur_vl = pwm - pwm / 3;
-      cur_vr = -pwm;
-      break;
-    case 4:     // Veer right
-      cur_vl = pwm;
-      cur_vr = -pwm + pwm / 3;
-      break;
-    case 5:     // Swirl
-      cur_vl = pwm;
-      cur_vr = -pwm;
-      break;
-  }
-  }*/
-
-/*
-  // convert gazebo action values to ranger action values
-  struct QAction normalize_speed(QAction action) {
-  float rg_min = -40;
-  float rg_max = 200;
-  float gz_min = -1.0;
-  float gz_max = 5.0;
-  float norm_vl = (action.vl - gz_min) / (gz_max - gz_min);
-  float new_vl = norm_vl * (rg_max - rg_min) + rg_min;
-  float norm_vr = (action.vr - gz_min) / (gz_max - gz_min);
-  float new_vr = -1 * (norm_vr * (rg_max - rg_min) + rg_min);
-  // return { map(action.vl, -1.0, 5.0, -200, 200), map(action.vr, -1.0, 5.0, -200, 200) };
-  return { new_vl, new_vr };
-  }
-
-  // convert gazebo distance values to ranger distance values
-  float normalize_dist(float rg_dist) {
-  float norm_dist = (rg_dist) / 400.0 * 2.75;
-  return norm_dist;
-  }
-*/
-
-/*
-  void write_csv(char* filepath, float** qtable) {
-  std::ofstream csv;
-  csv.open(filepath);
-
-  for (int i = 0; i < NUM_STATES; i++) {
-    float* row = qtable[i];
-    for (int j = 0; j < NUM_ACTIONS; j++) {
-      csv << row[j];
-      if (j < (NUM_ACTIONS - 1))
-        csv << ",";
-      else
-        csv << "\n";
-    }
-  }
-  }
-
-  float** read_from_csv(char* filepath) {
-  float** qtable = new float*[NUM_STATES;
-  std::ifstream csv;
-  csv.open(filepath);
-
-  while (!csv.eof()) {
-    float* row = new float[NUM_ACTIONS];
-    string line, val;
-    getline(csv, line);
-    std::stringstream ss(line);
-    while (getline(ss, val, ',')) {
-      row.push_back(std::stof(val));
-    }
-    qtable.push_back(row);
-  }
-  return qtable;
-  }
-*/
 
 // initializes the q-table
-void init_qtable(int num_states, int num_actions, const char* filepath, bool read_csv) {
-  qtable = new float*[num_states];
-  for (int i = 0; i != num_states; i++) {
-    qtable[i] = new float[num_actions];
-    for (int j = 0; j != num_actions; j++)
+void init_qtable() {
+  qtable = new float*[NUM_STATES];
+  for (int i = 0; i != NUM_STATES; i++) {
+    qtable[i] = new float[NUM_ACTIONS];
+    for (int j = 0; j != NUM_ACTIONS; j++)
       qtable[i][j] = 0.0;
   }
 }
@@ -342,17 +172,13 @@ float rand_uniform() {
 // choose an action
 // is chosen either optimally by the qtable or randomly
 // at a rate set by `eps`
-int choose_action(
-  //float** qtable,
-  int int_state,
-  float eps = EPS
-) {
+int choose_action() {
 
   int int_action;
   float rand_val = rand_uniform();
 
-  if (rand_val < eps) {
-    float* possible_actions = qtable[int_state];
+  if (rand_val < EPS) {
+    float* possible_actions = qtable[old_int_state];
     int int_action = 0;
     float max_action = 0.0;
     for (int i = 0; i < NUM_ACTIONS; i++) {
@@ -370,9 +196,7 @@ int choose_action(
 }
 
 // update the qtable with new information
-// update the qtable with new information
 void update_qtable() {
-
   float old_value = qtable[old_int_state][next_int_action];
   float max_q = -10000.0;
   for (int i = 0; i < NUM_ACTIONS; i++) {
@@ -381,6 +205,7 @@ void update_qtable() {
     }
   }
   float new_value = (1 - ALPHA) * old_value + ALPHA * (reward + GAMMA * max_q);
+
   qtable[old_int_state][next_int_action] = new_value;
 }
 
@@ -415,33 +240,7 @@ float get_reward() {
   return 0;
 }
 
-/*
-  // allow controlling robot via keyboard input
-  void keyboard_input(Robot* robot) {
-  switch((c=getch())) {
-    case KEY_UP:
-      cout << endl << "Up" << endl;//key up
-      robot->set_vel(2.0, 2.0);
-    case KEY_DOWN:
-      cout << endl << "Down" << endl;   // key down
-      robot->set_vel(-2.0, -2.0);
-    case KEY_LEFT:
-      cout << endl << "Left" << endl;  // key left
-      robot->set_vel(-2.0, 2.0);
-    case KEY_RIGHT:
-      cout << endl << "Right" << endl;  // key right
-      robot->set_vel(2.0, -2.0);
-    case "d":
-      cout << "done training" << endl;
-      TRAINING = FAlSE;
-    default:
-      cout << endl << "null" << endl;  // not arrow
-      break;
-  }
-  }
-*/
-
-void checkKey(float dist_f, float dist_r) {
+void checkKey() {
   if (dist_f < MAX_HALL && dist_r < MAX_HALL) {
     INSIDE = true;
   } else if (dist_f > 150 || dist_r > 150) {
@@ -465,7 +264,7 @@ void checkKey(float dist_f, float dist_r) {
 // setup arduino board
 void setup() {
   Serial.begin(9600);
-  print_qtable(qtable);
+  print_qtable();
   led.setpin(44);
   Serial.println("initializing qtable .....");
   //init_qtable(NUM_STATES, NUM_ACTIONS, CSV_FILEPATH, false);
@@ -502,103 +301,87 @@ void loop() {
   // increment tick
   TICK += 1;
 
-  // if training, only process keyboard input
-  // else, use qtable
-  // NOTE: keyboard functionality not yet enabled
-  if (TRAINING)
-    // keyboard_input(robot);
-    return;
-  else {
-
-    // decrease 'randomness' in q-learning actions after 5000 tick
-    if (TICK > 500) {
-      EPS = 0.7;
-      led.setColor(11, 0, 50, 0);
-      led.show();
-    }
-    // remove 'randomness' in q-learning actions after 15000 tick
-    if (TICK > 1000) {
-      EPS = 0.9;
-      led.setColor(10, 0, 0, 50);
-      led.show();
-    }
-
-    // check for a key
-    checkKey(dist_f, dist_r);
-
-    // cache qtable
-    /*
-      if (TICK % 1000 == 0)
-      write_csv(CSV_FILEPATH, STATE.qtable);
-    */
-
-    // find the best action to take next
-    // struct QState old_state = { dist_f, dist_r };
-    old_int_state = discretize_state(dist_f, dist_r);
-    next_int_action = choose_action(qtable, old_int_state);
-    realize_action(next_int_action);
-
-    // safety mechanism for robot getting stuck on wall
-    if (dist_f < 15) {
-      cur_vl = -200.0;
-      cur_vr = -100.0;
-      reward = 0;
-      MOTOR_L.setMotorPwm(cur_vl);
-      MOTOR_R.setMotorPwm(cur_vr);
-      //delay(200);
-    } else {
-
-      // perform that action
-      MOTOR_L.setMotorPwm(cur_vl);
-      MOTOR_R.setMotorPwm(cur_vr);
-
-      // measure reward from previous action
-      delay(150);
-      dist_f = ULTRA_F.distanceCm();
-      dist_r = ULTRA_R.distanceCm();
-      // struct QState cur_state = { dist_f, dist_r };
-      cur_int_state = discretize_state(dist_f, dist_r);
-      reward = get_reward();
-    }
-
-    // update q table
-    update_qtable();
-
-    if (key_count == 8) {
-      win();
-    }
-    // log activity
-    Serial.print("tick: ");
-    Serial.print(TICK);
-
-    Serial.print("\tstate: ");
-    Serial.print(cur_int_state);
-    Serial.print(", (");
-    Serial.print(dist_f);
-    Serial.print(", ");
-    Serial.print(dist_r);
-    Serial.print(")");
-
-    Serial.print("\taction: ");
-    Serial.print(next_int_action);
-    Serial.print(", (");
-    Serial.print(cur_vl);
-    Serial.print(", ");
-    Serial.print(cur_vr);
-    Serial.print(")");
-
-    Serial.print("\treward: ");
-    Serial.println(reward);
-
-    Serial.print(dist_f);
-    Serial.print(", ");
-    Serial.print(dist_r);
-    Serial.print("\tInside: ");
-    Serial.print(INSIDE);
-    Serial.print("\tDone: ");
-    Serial.print(DONE);
-    Serial.print("\tKey count: ");
-    Serial.println(key_count);
-    Serial.println(lineSensor.readSensors());
+  // decrease 'randomness' in q-learning actions after 5000 tick
+  if (TICK > 500) {
+    EPS = 0.7;
+    led.setColor(11, 0, 50, 0);
+    led.show();
   }
+  // remove 'randomness' in q-learning actions after 15000 tick
+  if (TICK > 1000) {
+    EPS = 0.9;
+    led.setColor(10, 0, 0, 50);
+    led.show();
+  }
+
+  // check for a key
+  checkKey();
+  
+  // find the best action to take next
+  old_int_state = discretize_state();
+  next_int_action = choose_action();
+  realize_action(next_int_action);
+
+  // safety mechanism for robot getting stuck on wall
+  if (dist_f < 15) {
+    cur_vl = -200.0;
+    cur_vr = -100.0;
+    reward = 0;
+    MOTOR_L.setMotorPwm(cur_vl);
+    MOTOR_R.setMotorPwm(cur_vr);
+    //delay(200);
+  } else {
+
+    // perform that action
+    MOTOR_L.setMotorPwm(cur_vl);
+    MOTOR_R.setMotorPwm(cur_vr);
+
+    // measure reward from previous action
+    delay(150);
+    dist_f = ULTRA_F.distanceCm();
+    dist_r = ULTRA_R.distanceCm();
+    // struct QState cur_state = { dist_f, dist_r };
+    cur_int_state = discretize_state();
+    reward = get_reward();
+  }
+
+  // update q table
+  update_qtable();
+
+  if (key_count == 8) {
+    win();
+  }
+  // log activity
+  Serial.print("tick: ");
+  Serial.print(TICK);
+
+  Serial.print("\tstate: ");
+  Serial.print(cur_int_state);
+  Serial.print(", (");
+  Serial.print(dist_f);
+  Serial.print(", ");
+  Serial.print(dist_r);
+  Serial.print(")");
+
+  Serial.print("\taction: ");
+  Serial.print(next_int_action);
+  Serial.print(", (");
+  Serial.print(cur_vl);
+  Serial.print(", ");
+  Serial.print(cur_vr);
+  Serial.print(")");
+
+  Serial.print("\treward: ");
+  Serial.println(reward);
+
+  Serial.print(dist_f);
+  Serial.print(", ");
+  Serial.print(dist_r);
+  Serial.print("\tInside: ");
+  Serial.print(INSIDE);
+  Serial.print("\tDone: ");
+  Serial.print(DONE);
+  Serial.print("\tKey count: ");
+  Serial.println(key_count);
+  Serial.println(lineSensor.readSensors());
 }
